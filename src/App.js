687548cb0737246b5a877eb1aded82895891f967
kanat <kijalbaev@gmail.com>
2021-05-05 12:30:31 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+    
+  }
+  
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+  
+  handleChange = event => {
+    const userInput = event.target.value.toLowerCase();
+    this.setState({ searchField: userInput });
+  };
+  
+  render() {
+    const { monsters } = this.state;
+    const userInput = this.state.searchField.toLowerCase();
+    const filteredMonsters = monsters.filter(monster => {
+        return monster.name.toLowerCase().includes(userInput);
+      },
+    );
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder="search monsters" handleChange={this.handleChange}/>
+        <CardList monsters={filteredMonsters}/>
+      
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './card.styles.css';
+
+class Card extends Component {
+  
+  render() {
+    return (
+      <div className="card-container">
+        <img alt="monster" src={`https://robohash.org/${this.props.monster.id}1?set=set2&size=180x180`}/>
+        <h2>{this.props.monster.name}</h2>
+        <p>{this.props.monster.email}</p>
+      </div>
+    );
+  }
+  
+}
+
+export default Card;
